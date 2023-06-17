d99d5f074052907ae415afcc5bbb4e39c610abe6
Someron Bakuli <someron.bakuli05@gmail.com>
2023-06-17 15:05:43 +0530

    Inro To dart

---
+++ hello.dart
@@ -0,0 +1,15 @@
+void main() {
+  print("Hello, Someron!");
+  int abc;
+  abc = 1;
+  print("ABC : ${abc}");
+  late int bc;
+  bc = 1;
+  print("BC : ${bc}");
+
+  int? k;
+  print("k : ${k}");
+
+  final DEV_NAME = "Someron Bakuli";
+  print(DEV_NAME);
+}
